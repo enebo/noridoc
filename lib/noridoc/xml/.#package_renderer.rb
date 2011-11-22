7535c6159455a19413a4a30d200c4cbfa7704c04
Thomas E. Enebo <tom.enebo@gmail.com>
2011-11-22 15:31:46 -0600

    Whoops did not save that file

--- lib/noridoc/xml/.#package_renderer.rb
+++
@@ -1,1 +0,0 @@
-enebo@mrpopo.234
--- lib/noridoc/xml/package_renderer.rb
+++ lib/noridoc/xml/package_renderer.rb
@@ -34,17 +34,6 @@ module NoriDoc
         end
       end
 
-      # Location relative to current directory we should move up to
-      # make package_path values work.  Note, on all_packages don't
-      # need an up_path so it overrides and returns ''
-      def calculate_up_path(name)
-          up_path = ''
-          (name.split('.').length - 1).times do 
-            up_path += '../'
-          end
-          up_path
-      end
-
       def render_package_class_list(io, indent, package, up_path)
         tag(io, :class_list, indent) do |indent2|
           package.all_classes.sort {|a, b| a.name <=> b.name}.each do |cls|
